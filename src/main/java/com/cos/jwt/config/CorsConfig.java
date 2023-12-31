3aa0873b648bbc72ffaff50ac14dad890fc11819
kimjaebeom98 <woqjadl6487@naver.com>
2023-12-31 19:53:21 +0900

    jwt를 위한 security 설정

---
+++ src/main/java/com/cos/jwt/config/CorsConfig.java
@@ -0,0 +1,28 @@
+package com.cos.jwt.config;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+//cors 설정
+@Configuration
+public class CorsConfig {
+	
+	@Bean
+	public CorsFilter corsFilter() {
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		CorsConfiguration config = new CorsConfiguration();
+		config.setAllowCredentials(true); // 내 서버가 응답할 때 json을 자바스크립트에서 처리할 수 있게 할지를 설정하는 것
+		config.addAllowedOrigin("*"); // 모든 ip에 응답을 허용
+		config.addAllowedHeader("*"); // 모든 header에 응답을 허용
+		config.addAllowedMethod("*"); // 모든 http method에 대한 응답을 허용
+		// 해당 Url 패턴으로 들어오는 모든 요청들은 이 config 설정을 따라가도록 등록
+		source.registerCorsConfiguration("/api/**", config);
+		return new CorsFilter(source);
+	}
+}
---
+++ src/main/java/com/cos/jwt/config/SecurityConfig.java
@@ -0,0 +1,41 @@
+package com.cos.jwt.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.filter.CorsFilter;
+
+import lombok.RequiredArgsConstructor;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+	
+	private final CorsFilter corsFilter;
+	
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		http.csrf().disable();
+		// 세션 사용 X
+		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+		.and()
+		.addFilter(corsFilter) // 사용자 인증이 필요없을 경우의 요청들은 @CrossOrigin을 컨트롤러에 붙이면 되는데, 인증이 필요한 시큐리티의 경우는 시큐리티 필터에 등록해야함
+		.formLogin().disable() // form태그 만들어서 로그인 x
+		.httpBasic().disable() // httpBasic이란 header의 Authorization 영역에 ID, PW를 담아서 요청할 때 마다 인증 가능하게 함 근데 http는 암호화가 안되니 https를 쓰긴함 
+		                                  // 그럼 왜 disable 했냐면 Authorization영역에 우리는 token을 담을거임 id, pw보다는 token이 요청되는게 그나마 나아서 그런가..? 암튼 token을 달고 요청하는게 bearer 방식
+		.authorizeRequests()
+		.antMatchers("/api/v1/user/**")
+		.hasAnyRole("USER", "ADMIN", "MANAGER")
+		.antMatchers("/api/v1/manager/**")
+		.hasAnyRole("ADMIN", "MANAGER")
+		.antMatchers("/api/v1/admin/**")
+		.hasAnyRole("ADMIN")
+		.anyRequest().permitAll();
+		
+		return http.build();
+	}
+}
